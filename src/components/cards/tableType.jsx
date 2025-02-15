5d083c2179c4ceaf8a71430900f426e1101c11c1
b_yusupov <bunyodyusupov0@gmail.com>
2025-02-15 18:05:07 +0500

    fix

--- src/components/cards/tableType.jsx
+++ src/components/cards/tableType.jsx
@@ -41,14 +41,14 @@ export const NPK_DATA = [
     id: 1,
     titleUz: 'GreenPlus NPK',
     descriptionUz: "Green Plus NPK белгиланган нисбатда барча ўсимликлар ва мевали дарахтларда хавфсиз ишлатиладиган мувозанатли ўғитдир.",
-    productPicture: "/public/npk/image.png",
+    productPicture: "/npk/image.png",
     
   },
   {
     id: 2,
     titleUz: 'POLIFERT NPK',
     descriptionUz: "Poly Fert NPK белгиланган нисбатда барча ўсимликлар ва мевали дарахтларда хавфсиз ишлатиладиган мувозанатли ўғитдир. Таркибидаги қўшимча магний ва ферментлар туфайли озиқ моддаларнинг",
-    productPicture: "/public/npk/image.png"
+    productPicture: "/npk/image.png"
   }
 ]
 
