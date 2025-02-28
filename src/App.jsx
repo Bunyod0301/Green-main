7112ca1a350cfdf47676bc8a77b2c3573469a897
b_yusupov <bunyodyusupov0@gmail.com>
2025-02-28 11:12:14 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PlantNutritionView from "./pages/products/plant-nutrition/view/PlantNutri
 import PlantNutritionNpk from "./pages/products/plant-nutrition/view/NpkView";
 import Catalog from "./pages/catalog/Catalog";
 import Liflet from "./pages/liflet/Liflet";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 function App() {
 
   return (
@@ -35,6 +36,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/news" element={<News />} />
       </Routes>
+      <ScrollToTop />
       <Footer />
     </>
   );
