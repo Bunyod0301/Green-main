49789a4e9131dae1a253a206b2d11681edbf6deb
b_yusupov <bunyodyusupov0@gmail.com>
2025-03-03 13:40:06 +0500

    prod

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -3,8 +3,8 @@ import { create } from "zustand";
 
 // axios instansiyasini yaratish
 export const api = axios.create({
-  baseURL: "https://agro-backkend.vercel.app/api"
-  // baseURL: "http://167.172.69.159:8080"
+  // baseURL: "https://agro-backkend.vercel.app/api"
+  baseURL: "http://167.172.69.159:8080"
 });
 
 const useStore = create((set) => ({
