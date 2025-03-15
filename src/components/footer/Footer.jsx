f870d4c8fdb820b5172525abd4f14bc468476609
b_yusupov <bunyodyusupov0@gmail.com>
2025-03-15 16:28:54 +0500

    fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -17,29 +17,31 @@ const Footer = () => {
   };
 
   return (
+    <>
     <div className="w-full bg-green-600 text-white py-10 px-5 text-sm 2xl:text-2xl 4xl:text-3xl">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 text-center md:text-left">
         {/* Logo Section */}
         {/* <div className="rounded-sm rounded-tr-[55px] rounded-bl-[30px] bg-white p-1 h-[70px] w-[210px]"> */}
             <div>
               <NavLink to="/" className="flex items-center justify-center md:justify-start space-x-3 rtl:space-x-reverse">
-                <div className="flex items-center justify-between gap-2 bg-green-100 p-3 rounded">
-                  <img
-                    src={logo}
-                    alt="logo"
-                    className="rounded-[17px] w-[40px] h-[40px] md:w-[80px] md:h-[55px] xl:h-[65px]"
-                  />
-                  <div className="w-full text-end">
-                    <h3 className="font-bold text-[#0E644D] text-start text-sm md:text-lg xl:text-2xl 4xl:text-3xl">
-                      Agro<span className="text-green-500">Com</span>
-                    </h3>
-                    <span className="text-green-600 text-end w-full font-medium text-xs md:text-sm xl:text-xl 4xl:text-2xl">
-                      Ishonchli himoya
-                    </span>
-                  </div>
+              <div className="flex items-center justify-between gap-2 bg-green-700 p-3 rounded">
+                <img
+                  src={logo}
+                  alt="logo"
+                  className="rounded-[17px] w-[40px] h-[40px] md:w-[90px] md:h-[65px] xl:h-[75px]"
+                />
+                <div className="w-full text-end ml-2">
+                  {/* <h3 className="font-bold text-[#0E644D] text-start text-sm md:text-lg xl:text-2xl 4xl:text-3xl"> */}
+                  <h3 className="font-bold text-white text-start text-xl md:text-2xl xl:text-3xl 4xl:text-4xl">
+                    Agro<span className="text-white">Com</span>
+                  </h3>
+                  <span className="text-white text-end w-full font-medium text-xs md:text-sm xl:text-sm 4xl:text-xl">
+                    Ishonchli himoya
+                  </span>
                 </div>
+              </div>
               </NavLink>
-              <p className='font-bold text-sm md:text-lg 2xl:text-xl 4xl:text-2xl mt-2'>СИЗГА ЮҚОРИ ҲОСИЛ ОЛИШИНГИЗДА КЎМАКЛАШАМИЗ</p>
+              
             </div>
           {/* </div> */}
 
@@ -77,6 +79,10 @@ const Footer = () => {
       {/* Modal */}
       {isOpen && <ModalForm toggleModal={toggleModal} />} 
     </div>
+    <div className="py-3 bg-green-800 text-center text-white">
+      <p className='font-bold text-sm md:text-lg 2xl:text-xl 4xl:text-2xl mt-2'>СИЗГА ЮҚОРИ ҲОСИЛ ОЛИШИНГИЗДА КЎМАКЛАШАМИЗ</p>
+    </div>
+  </>
   );
 };
 
